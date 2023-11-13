317b476b61322ae2e3d8c40d793e31153979be84
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-11-13 16:17:22 +0100

    created the individual chat view

---
+++ lib/screens/individual_chat_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IndividualChatScreen extends StatefulWidget {
+  const IndividualChatScreen({super.key});
+
+  @override
+  State<IndividualChatScreen> createState() => _IndividualChatScreenState();
+}
+
+class _IndividualChatScreenState extends State<IndividualChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
