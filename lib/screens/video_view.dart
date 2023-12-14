1955775fff8b9d44f8c4787ed47e3b48bf6b0911
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-12-14 14:27:54 +0100

    created the Video view screen

---
+++ lib/screens/video_view.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class VideoViewScreen extends StatefulWidget {
+  const VideoViewScreen({super.key, required this.path});
+  final String path;
+
+  @override
+  State<VideoViewScreen> createState() => _VideoViewScreenState();
+}
+
+class _VideoViewScreenState extends State<VideoViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.crop_rotate),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.emoji_emotions_outlined),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.title),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.edit),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: double.infinity,
+            child: Image.file(
+              File(widget.path),
+              fit: BoxFit.cover,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              color: Colors.black38,
+              child: TextFormField(
+                style: const TextStyle(color: Colors.white),
+                maxLines: 6,
+                minLines: 1,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.add_photo_alternate,
+                    color: Colors.white,
+                  ),
+                  suffixIcon: CircleAvatar(
+                    backgroundColor: Colors.teal,
+                    radius: 26,
+                    child: Icon(
+                      Icons.check,
+                      color: Colors.white,
+                      size: 27,
+                    ),
+                  ),
+                  border: InputBorder.none,
+                  hintText: "Add caption ....",
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
